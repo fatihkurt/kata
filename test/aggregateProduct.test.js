c863c972daa67e0db079b84a02c5de97ae3f74da
Fatih Kurt <kurt.fth@gmail.com>
2022-01-06 15:10:49 +0300

    Test added

---
+++ test/aggregateProduct.test.js
@@ -0,0 +1,38 @@
+const assert = require("assert");
+
+const aggregateProduct = require('./../')
+
+describe("aggregateProduct", () => {
+  const productLayers = [
+    {
+      title: "Red pants",
+      description:
+        "Red pants are highly addictive, and made out of awesome stuff only!",
+    },
+    {
+      title: "Red pants from manufacturor title",
+      tags: ["red", "pants", "awesome"],
+      images: [
+        { src: "https://picsum.photos/200" },
+        { src: "https://picsum.photos/350" },
+      ],
+    },
+  ];
+
+  const expectedOutput = {
+    title: "Red pants",
+    description:
+      "Red pants are highly addictive, and made out of awesome stuff only!",
+    tags: ["red", "pants", "awesome"],
+    images: [
+      { src: "https://picsum.photos/200" },
+      { src: "https://picsum.photos/350" },
+    ],
+  };
+
+  it("should aggregate product details", () => {
+    const output = aggregateProduct(productLayers);
+
+    assert.equal(output, expectedOutput);
+  });
+});
